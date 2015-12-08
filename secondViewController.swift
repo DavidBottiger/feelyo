9c24a128d714dbcb4d6fdb2b602f9eff79d63fd6
Robinhiio <robinlarsson@Robins-MacBook-Air.local>
2015-12-08 13:48:29 +0100

    Parse files
    Added parse files

--- secondViewController.swift
+++ secondViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import UIKit
+import Parse
 
 class secondViewController: UIViewController {
+    
+    var gifId = String()
+    var gifLikes = Int()
+    var gifUsers = [String]()
+    var gifObjectId = String()
+    var backgroundColor = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,4 +43,85 @@ class secondViewController: UIViewController {
     @IBAction func backButton(sender: AnyObject) {
     performSegueWithIdentifier("firstVC", sender: self)
     }
+    
+    func getGifInfo()
+    {
+        var user = PFUser.currentUser()
+        var query = PFQuery(className:"Gifs")
+        query.whereKey("gifId", equalTo:gifId)
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                print("Successfully retrieved \(objects!.count) Gifs")
+                if let objects = objects {
+                    for object in objects {
+                        self.gifId = object["gifId"] as! String
+                        self.gifObjectId = object["objectId"] as! String
+                        self.gifLikes = object["likes"] as! Int
+                        self.gifUsers = object["likedByUsers"] as! [String]
+                        
+                        if (self.gifUsers.contains((user?.username)!))
+                        {
+                        //Likebutton.hidden = true
+                        //Dislikebutton.hidden = false
+                        }else{
+                        //Likebutton.hidden = false
+                        //Dislikebutton.hidden = true
+                        }
+                        
+                    }
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.userInfo)")
+            }
+        }
+    }
+    @IBAction func likeButton(sender: UIButton)
+    {
+        var user = PFUser.currentUser()
+        var query = PFQuery(className:"Gifs")
+        query.whereKey("gifId", equalTo:gifId)
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) Gifs")
+                // Do something with the found objects
+                if let objects = objects {
+                    for object in objects {
+                        self.gifLikes = object["likes"] as! Int
+                        self.gifUsers = object["likedByUsers"] as! [String]
+                        
+                    }
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.userInfo)")
+            }
+        
+        var query2 = PFQuery(className:"Gifs")
+        query2.getObjectInBackgroundWithId(self.gifObjectId) {
+            (gifInfo: PFObject?, error: NSError?) -> Void in
+            if error != nil {
+                print(error)
+            } else if let gifInfo = gifInfo {
+
+                gifInfo.addObject(user!.username!, forKey: "likedByUsers")
+                gifInfo["likes"] = self.gifLikes + 1
+                //likebutton.hidden = true
+                //dislikebutton.hidden = false
+                gifInfo.saveInBackground()
+                print("Gif Liked")
+                
+            }
+        }
+      }
+    }
+    
+    @IBAction func dislikeButton(sender: UIButton) {
+    }
+    
 }
